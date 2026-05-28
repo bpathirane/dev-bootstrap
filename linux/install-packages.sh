bd556b2aceef368532eb8eb56bd1df99648ec54e
Buddhi Pathirane <2478087+bpathirane@users.noreply.github.com>
2026-05-27 22:36:43 -0400

    add libxml2-utils to bootstrap

--- linux/install-packages.sh
+++ linux/install-packages.sh
@@ -8,7 +8,7 @@ apt_update_if_stale
 
 BASE_PACKAGES=(
   age bat bison build-essential ca-certificates cifs-utils curl direnv fd-find
-  git gnupg htop jq krb5-user libevent-dev lsb-release ncurses-dev pkg-config ripgrep unzip wslu zsh
+  git gnupg htop jq krb5-user libevent-dev libxml2-utils lsb-release ncurses-dev pkg-config ripgrep unzip wslu zsh
 )
 
 for pkg in "${BASE_PACKAGES[@]}"; do
